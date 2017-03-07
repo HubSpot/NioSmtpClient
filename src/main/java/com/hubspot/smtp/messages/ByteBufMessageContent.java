fb26b95d0ff439aff469ead5bf0998027cc7a5da
Michael O'Brien <mobrien@hubspot.com>
2017-03-07 11:43:39 +0000

    Don't terminate messages unless required

--- src/main/java/com/hubspot/smtp/messages/ByteBufMessageContent.java
+++ src/main/java/com/hubspot/smtp/messages/ByteBufMessageContent.java
@@ -1,22 +1,41 @@
 package com.hubspot.smtp.messages;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 
 public class ByteBufMessageContent extends MessageContent {
+  private static final byte CR = '\r';
+  private static final byte LF = '\n';
+  private static final byte[] CR_LF = {CR, LF};
+  private static final ByteBuf CR_LF_BUFFER = Unpooled.unreleasableBuffer(Unpooled.wrappedBuffer(CR_LF));
+
   private final ByteBuf buffer;
   private final int size;
 
   public ByteBufMessageContent(ByteBuf buffer, MessageContentEncoding encoding) {
-    this.buffer = encoding == MessageContentEncoding.REQUIRES_DOT_STUFFING ? wrap(buffer) : buffer;
+    if (encoding == MessageContentEncoding.REQUIRES_DOT_STUFFING) {
+      this.buffer = dotStuff(buffer);
+    } else {
+      this.buffer = isTerminated(buffer) ? buffer : terminate(buffer);
+    }
+
     this.size = this.buffer.readableBytes();
   }
 
-  private ByteBuf wrap(ByteBuf buffer) {
-    int length = buffer.readableBytes();
-    boolean isTerminated = length >= 2 && buffer.getByte(length - 2) == '\r' && buffer.getByte(length - 1) == '\n';
+  private static ByteBuf terminate(ByteBuf buffer) {
+    return buffer.alloc()
+        .compositeBuffer(2)
+        .addComponents(true, buffer, CR_LF_BUFFER.slice());
+  }
 
+  private static ByteBuf dotStuff(ByteBuf buffer) {
     return DotStuffing.createDotStuffedBuffer(buffer.alloc(), buffer, null,
-        isTerminated ? MessageTermination.DO_NOT_TERMINATE : MessageTermination.ADD_CRLF);
+        isTerminated(buffer) ? MessageTermination.DO_NOT_TERMINATE : MessageTermination.ADD_CRLF);
+  }
+
+  private static boolean isTerminated(ByteBuf buffer) {
+    int length = buffer.readableBytes();
+    return length >= 2 && buffer.getByte(length - 2) == '\r' && buffer.getByte(length - 1) == '\n';
   }
 
   @Override
