bf731ce3a283beb5172c264b683496cd8d990bed
Michael O'Brien <mobrien@hubspot.com>
2017-05-03 15:39:51 +0100

    Return a fixed estimate when streams cannot mark

--- src/test/java/com/hubspot/smtp/messages/InputStreamMessageContentTest.java
+++ src/test/java/com/hubspot/smtp/messages/InputStreamMessageContentTest.java
@@ -1,6 +1,12 @@
 package com.hubspot.smtp.messages;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.OptionalInt;
+
+import org.junit.Test;
 
 import com.google.common.io.ByteSource;
 
@@ -8,5 +14,18 @@ public class InputStreamMessageContentTest extends MessageContentTest {
   @Override
   protected MessageContent createContent(byte[] bytes) {
     return new InputStreamMessageContent(ByteSource.wrap(bytes), OptionalInt.of(bytes.length), MessageContentEncoding.UNKNOWN);
+  }
+
+  @Test
+  public void itUsesAFixedValueForStreamsThatDoNotSupportMark() {
+    ByteArrayInputStream stream = new ByteArrayInputStream("hello".getBytes(StandardCharsets.UTF_8)) {
+      @Override
+      public boolean markSupported() {
+        return false;
+      }
+    };
+
+    MessageContent content = new InputStreamMessageContent(() -> stream, OptionalInt.empty(), MessageContentEncoding.UNKNOWN);
+    assertThat(content.get8bitCharacterProportion()).isEqualTo(0.1F);
   }
 }
